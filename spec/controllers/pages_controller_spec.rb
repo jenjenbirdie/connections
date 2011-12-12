525572d2af96dcafed4ad469ee10669c968e41f4
jenjenbirdie <jennifer.birdie@gmail.com>
2011-12-12 00:50:16 -0800

    Done with dynamic pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+	it "should have the right title" do
+		get 'home'
+		response.should have_selector("title", :content => " | Home")
+	end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +19,10 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+	it "should have the right title" do
+		get 'contact'
+		response.should have_selector("title", :content => " | Contact")
+	end
   end
 
   describe "GET 'about'" do
@@ -21,6 +30,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+	it "should have the right title" do
+		get 'about'
+		response.should have_selector("title", :content => " | About")
+	end
   end
   
 end
