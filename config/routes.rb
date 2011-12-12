44dd7cd9fe8f7b40f57fc3d55eb33679c2face83
jenjenbirdie <jennifer.birdie@gmail.com>
2011-12-12 12:22:44 -0800

    Done with adding help

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Connections::Application.routes.draw do
 
   get "pages/contact"
   
-    get "pages/about"
+  get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
