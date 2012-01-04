7e13e71598cea04e505f02706bb7fca327866355
jenjenbirdie <jennifer.birdie@gmail.com>
2012-01-04 13:19:30 -0800

    Done with integration testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Connections::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  #get "pages/home"
+  match '/contact',  :to => 'pages#contact'
+  match '/about',  :to => 'pages#about'
+  match '/help',  :to => 'pages#help'
   
-  get "pages/about"
-  
-  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,6 +55,7 @@ Connections::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
