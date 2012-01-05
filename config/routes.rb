61c8b1b4f7938727e5fe537897896b21d52ae6f1
jenjenbirdie <jennifer.birdie@gmail.com>
2012-01-05 00:28:54 -0800

    Finished sighup user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Connections::Application.routes.draw do
+  get "users/new"
+  match '/signup', :to => 'users#new'
+
   #get "pages/home"
   match '/contact',  :to => 'pages#contact'
   match '/about',  :to => 'pages#about'
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,4 +17,8 @@ describe "LayoutLinks" do
 	get '/help'
 	response.should have_selector('title', :content => "Help")
   end
+  it "should have a Help page at '/signup'" do
+	get '/signup'
+	response.should have_selector('title', :content => "Sign up")
+  end
 end
