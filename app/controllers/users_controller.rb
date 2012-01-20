525f991224a2ef0abfd1af133daa19bd6ff6793e
jenjenbirdie <jennifer.birdie@gmail.com>
2012-01-19 17:05:37 -0800

    Done with signing-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new  #correcposd to new.html.erb w/o this, error.
 	@title = "Sign up"
   end
   def show
 	@user = User.find(params[:id])
 	@title = @user.name
   end
-
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+		#Handle a successful save.
+	else
+		@title = "Sign up"
+		render 'new'
+	end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,12 @@ describe UsersController do
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 	
 	it "should have the right title" do
-		get 'new'
+		get :new
 		response.should have_selector("title", :content => "Sign up")   #has to be exact string, spec error if else
 	end
   end
@@ -38,7 +38,26 @@ describe UsersController do
 		get :show, :id => @user
 		response.should have_selector("h1>img", :class => "gravatar")
 	end
-	
+  end
+  describe "POST 'create'" do
+	describe "failure" do
+		before(:each) do
+			@attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+		end
+		it "should not create a user" do
+			lambda do
+				post :create, :user => @attr
+			end.should_not change(User, :count)
+		end
+		it "should have the right title" do
+			post :create, :user => @attr
+			response.should have_selector("title", :content => "Sign up" )
+		end
+		it "should render the new page" do
+			post :create, :user => @attr
+			response.should render_template('new')
+		end
+	end
   end
  
 end
