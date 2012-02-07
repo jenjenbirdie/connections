88f8b5d86eacc82a0e6eb6fdee170788df40d8ea
jenjenbirdie <jennifer.birdie@gmail.com>
2012-02-07 13:24:34 -0800

    Done with protecting-pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def new
     @user = User.new  #correcposd to new.html.erb w/o this, error.
 	@title = "Sign up"
@@ -22,11 +25,11 @@ class UsersController < ApplicationController
 	end
   end
   def edit
-	@user = User.find(params[:id])
+	#@user = User.find(params[:id])  remove coz already in correct_user p. 381 ror tutorial
 	@title = "Edit user"
   end
   def update
-	@user = User.find(params[:id])
+	#@user = User.find(params[:id])
 	if @user.update_attributes(params[:user])
 		flash[:success] = "Profile updated."
 		redirect_to @user
@@ -35,4 +38,14 @@ class UsersController < ApplicationController
 		render 'edit'
 	end
   end
+  
+  private
+	def authenticate
+		deny_access unless signed_in?
+	end
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
 end
