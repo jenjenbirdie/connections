f51ff893ecbd8dceeb574c412b0c4ce5cf8f90c5
jenjenbirdie <jennifer.birdie@gmail.com>
2012-02-08 13:52:47 -0800

    Done with partial destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
   
   def index
 	@title = "All users"
@@ -8,6 +9,7 @@ class UsersController < ApplicationController
 	@users = User.paginate(:page => params[:page])
   end
   
+ 
   def new
     @user = User.new  #correcposd to new.html.erb w/o this, error.
 	@title = "Sign up"
@@ -45,6 +47,12 @@ class UsersController < ApplicationController
 	end
   end
   
+   def destroy
+	User.find(params[:id]).destroy
+	flash[:success] = "User destroyed"
+	redirect_to users_path
+  end
+  
   private
 	def authenticate
 		deny_access unless signed_in?
@@ -53,5 +61,9 @@ class UsersController < ApplicationController
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to(root_path) unless current_user?(@user)
+	end
+	
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 end
