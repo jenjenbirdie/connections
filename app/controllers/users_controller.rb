a9a96b55b61345a6a47b62f01ee43bc98bd45e1e
jenjenbirdie <jennifer.birdie@gmail.com>
2012-02-17 20:26:00 -0800

    Done with show-microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   end
   def show
 	@user = User.find(params[:id])
+	@microposts = @user.microposts.paginate(:page => params[:page])
 	@title = @user.name
   end
   def create
